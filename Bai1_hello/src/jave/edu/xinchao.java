e3a13fe1ca501e0ce9eee7501deb79db67bcdae1
Doan Thanh Tu <tu.dt.64cntt@ntu.edu.vn>
2025-02-12 20:08:39 +0700

    vi du
    test demo

---
+++ Bai1_hello/src/jave/edu/xinchao.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package jave.edu;
+
+/**
+ * 
+ */
+public class xinchao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("xin chao");
+
+	}
+
+}
