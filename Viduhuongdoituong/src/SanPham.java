70415d71bd8d7bd0c47aca5c32271224792cd686
Doan Thanh Tu <tu.dt.64cntt@ntu.edu.vn>
2025-02-16 19:28:17 +0700

    Update SanPham.java

--- Viduhuongdoituong/src/SanPham.java
+++ Viduhuongdoituong/src/SanPham.java
@@ -1,4 +1,53 @@
+class SanPham {
+    private int maSP;
+    private String tenSP;
+    private String loaiSP;
+    private String anhSP;
 
-public class SanPham {
+    public SanPham() {}
 
+    public SanPham(int maSP, String tenSP, String loaiSP, String anhSP) {
+        this.maSP = maSP;
+        this.tenSP = tenSP;
+        this.loaiSP = loaiSP;
+        this.anhSP = anhSP;
+    }
+
+    public void setMaSP(int maSP) {
+        this.maSP = maSP;
+    }
+
+    public void setTenSP(String tenSP) {
+        this.tenSP = tenSP;
+    }
+
+    public void setLoaiSP(String loaiSP) {
+        this.loaiSP = loaiSP;
+    }
+
+    public void setAnhSP(String anhSP) {
+        this.anhSP = anhSP;
+    }
+
+    public int getMaSP() {
+        return maSP;
+    }
+
+    public String getTenSP() {
+        return tenSP;
+    }
+
+    public String getLoaiSP() {
+        return loaiSP;
+    }
+
+    public String getAnhSP() {
+        return anhSP;
+    }
+
+    @Override
+    public String toString() {
+        return "[Mã sản phẩm: " + maSP + "\nTên sản phẩm: " + tenSP + 
+               "\nLoại sản phẩm: " + loaiSP + "\nẢnh sản phẩm: " + anhSP + "]";
+    }
 }
